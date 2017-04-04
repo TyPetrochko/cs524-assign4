9b9b366098dfe4899c010ab6372698c66298b29f
Tyler Hutchinson Petrochko of group cpsc424 <thp8@compute-33-1.local>
2017-04-04 08:16:08 -0400

    Directory ready to go

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 module load Langs/Intel/15
 module load Langs/Intel/15 MPI/OpenMPI/1.8.6-intel15
 
-mpiexec -n 8 parallel < /home/fas/hpcprog/ahs3/cpsc424/assignment4/data/actualdata1 > ./parallel_data1
-mpiexec -n 8 parallel < /home/fas/hpcprog/ahs3/cpsc424/assignment4/data/actualdata2 > ./parallel_data2
-mpiexec -n 8 parallel < /home/fas/hpcprog/ahs3/cpsc424/assignment4/data/actualdata3 > ./parallel_data3
-mpiexec -n 8 parallel < /home/fas/hpcprog/ahs3/cpsc424/assignment4/data/actualdata4 > ./parallel_data4
+mpiexec -n 8 parallel < /home/fas/hpcprog/ahs3/cpsc424/assignment4/data/actualdata1 > ./parallel_data1.txt
+mpiexec -n 8 parallel < /home/fas/hpcprog/ahs3/cpsc424/assignment4/data/actualdata2 > ./parallel_data2.txt
+mpiexec -n 8 parallel < /home/fas/hpcprog/ahs3/cpsc424/assignment4/data/actualdata3 > ./parallel_data3.txt
+mpiexec -n 8 parallel < /home/fas/hpcprog/ahs3/cpsc424/assignment4/data/actualdata4 > ./parallel_data4.txt
 
 ./serial < /home/fas/hpcprog/ahs3/cpsc424/assignment4/data/actualdata1 > ./serial_data1.txt
 ./serial < /home/fas/hpcprog/ahs3/cpsc424/assignment4/data/actualdata2 > ./serial_data2.txt
